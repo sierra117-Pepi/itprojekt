1ab38224933dce3d1304be0383c145e25920c8ec
aundroid <rabitsch.andreas@gmail.com>
2017-06-06 18:19:11 +0200

    chat

---
+++ Prototype/JavaScript/CustomFiles/chatWorker.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    var save = document.getElementById("cmd").addEventListener("click", savePDF);
+
+});
+
+function savePDF() {
+    var doc = new jsPDF();
+    doc.fromHTML($('#content').html(), 15, 15, {
+        'width': 500,
+    });
+    doc.save('Nachrichten.pdf');
+
+}
+
+
+
+
+
+/*
+var doc = new jsPDF();
+var specialElementHandlers = {
+    '#editor': function(element, renderer) {
+        return true;
+    }
+};
+
+$('#cmd').click(function() {
+    doc.fromHTML($('#content').html(), 15, 15, {
+        'width': 170,
+        'elementHandlers': specialElementHandlers
+    });
+    doc.save('Nachrichten.pdf');
+});
+*/
