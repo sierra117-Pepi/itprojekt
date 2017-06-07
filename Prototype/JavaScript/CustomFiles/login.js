5d609038a73ae2596b023c09b4ce01bf96810cfe
Petko Dragoev <petko_dragoev@yahoo.com>
2017-06-07 15:10:53 +0200

    admin account

--- Prototype/JavaScript/CustomFiles/login.js
+++ Prototype/JavaScript/CustomFiles/login.js
@@ -8,6 +8,11 @@ var worker = {
     email: "test@worker.com",
     password: "testworker"
 };
+var admin = {
+    name: "testAdmin",
+    email: "test@admin.com",
+    password: "testadmin"
+};
 
 window.onload = function () {
     addActionListeners();
@@ -19,19 +24,13 @@ function addActionListeners() {
         var password = document.getElementById("password").value;
 
         if (user.email === email && user.password === password) {
-            openWindowUser();
+            window.location.href = "../HTML/UserSites/indexUser.html";
         } else if (worker.email === email && worker.password === password) {
-            openWindowWorker();
+            window.location.href = "../HTML/WorkerSites/indexWorker.html";
+        } else if (admin.email === email && admin.password === password) {
+            window.location.href = "../HTML/AdminSites/indexAdmin.html";
         } else {
             alert("Invalid data!");
         }
     });
-}
-
-function openWindowUser() {
-    window.location.href = "../HTML/UserSites/indexUser.html";
-}
-
-function openWindowWorker() {
-    window.location.href = "../HTML/WorkerSites/indexWorker.html";
 }
