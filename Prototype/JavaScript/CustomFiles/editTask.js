afdff05a09d23a999db1d43f8a5b58b31ad8bff7
aundroid <rabitsch.andreas@gmail.com>
2017-06-07 17:39:47 +0200

    meine reklamationen

---
+++ Prototype/JavaScript/CustomFiles/editTask.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    var claimButton = document.getElementById("retButton").addEventListener("click", sendRetoure);
+
+  });
+
+  function sendRetoure(){
+    window.location.href = "../../HTML/UserSites/messagesUser.html";
+  }
